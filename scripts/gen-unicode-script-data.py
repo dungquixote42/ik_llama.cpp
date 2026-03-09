084edc6fcd82e9b1dab82ee4eb889f9b701b155a
banana split <hyunsoo.david.kim@gmail.com>
2026-03-08 21:50:53 -0400

    add python script and generated uscript data

---
+++ scripts/gen-unicode-script-data.py
@@ -0,0 +1,92 @@
+import requests
+
+
+MAX_CODEPOINTS = 0x110000
+
+SCRIPTS_DATA_URL = "https://www.unicode.org/Public/UCD/latest/ucd/Scripts.txt"
+
+
+def get_script_data():
+    res = requests.get(SCRIPTS_DATA_URL)
+    res.raise_for_status()
+    data = res.content.decode()
+    
+    script_data = []
+    
+    for line in data.splitlines():
+        line = line.split()
+        if len(line) <= 1 or line[0] == "#":
+            continue
+        assert line[1] == ";"
+        assert line[3] == "#"
+
+        cpt = line[0].split("..")
+        if len(cpt) == 1:
+            cpt += cpt
+        cpt_lower, cpt_upper = cpt
+
+        cpt_lower = int(cpt_lower, 16)
+        assert cpt_lower < MAX_CODEPOINTS
+
+        cpt_upper = int(cpt_upper, 16)
+        assert cpt_upper < MAX_CODEPOINTS
+
+        script = line[2].lower()
+
+        script_data.append([script, cpt_lower, cpt_upper])
+
+    script_data.sort(key=lambda x: x[1])
+
+    merged_data = [script_data[0]]
+    for sd in script_data[1:]:
+        if merged_data[-1][0] == sd[0] and merged_data[-1][2] + 1 == sd[1]:
+            merged_data[-1][2] = sd[2]
+        else:
+            merged_data.append(sd)
+
+    script_data = {}
+    for md in merged_data:
+        if md[0] in script_data:
+            script_data[md[0]].append([md[1], md[2]])
+        else:
+            script_data[md[0]] = [[md[1], md[2]]]
+
+    return script_data
+
+
+# def cpt_to_base16(cpt):
+#     return int(chr(cpt).encode("utf-8").hex(), 16)
+
+
+# Generate 'unicode-script-data.cpp':
+#   python ./scripts//gen-unicode-script-data.py > unicode-script-data.cpp
+
+def out(line=""):
+    print(line, end='\n')  # noqa
+
+
+out("""\
+// generated with scripts/gen-unicode-scripts-data.py
+
+#include "unicode-data.h"
+
+#include <cstdint>
+#include <string>
+#include <unordered_map>
+#include <vector>
+""")
+
+out("constexpr std::unordered_map<std::string, std::pair<std::vector<uint32_t>, std::vector<uint32_t>> unicode_scripts = {")
+
+script_data = get_script_data()
+for script in script_data:
+    out("{ \"%s\", { {" % script)
+    for data in script_data[script]:
+        # out("    { 0x%06X, 0x%06X }," % (data[0], data[1]))
+        out("    0x%06X," % data[0])
+    out("}, {")
+    for data in script_data[script]:
+        out("    0x%06X," % data[1])
+    out("} } },")
+
+out("};")
