7b488f3194ea2d46d072d1a8659a91af97a50a45
banana split <hyunsoo.david.kim@gmail.com>
2026-03-13 00:51:55 -0400

    delete old comments

--- src/unicode.h
+++ src/unicode.h
@@ -46,9 +46,7 @@ struct unicode_cpt_flags {
     }
 };
 
-// bool unicode_utf8_in_uscripts(std::string utf8, std::vector<std::string> uscripts);
 bool unicode_utf8_in_uscripts(const std::string& utf8, const std::vector<std::string>& uscripts);
-// bool unicode_utf8_in_uscripts(void* utf8, void* uscripts);
 
 size_t unicode_len_utf8(char src);
 
